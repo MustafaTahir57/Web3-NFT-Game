d83a8d6735f3937ac90c501d372881ada8345a75
Mustafa Tahir <mustafatahir802@gmail.com>
2023-07-22 07:12:44 +0500

    Border have been removed

--- client-starter/src/components/PlayerInfo.jsx
+++ client-starter/src/components/PlayerInfo.jsx
@@ -10,7 +10,7 @@ const marginindexing = (index) => index !== healthPoints - 1 ? 'mr-1' : 'mr-0'
 
 const PlayerInfo = ({ player, playerIcon, mt }) => {
   return (
-    <div className={`${styles.flexCenter} ${mt ? 'mt-4' : 'mb-4'} border-4`}>
+    <div className={`${styles.flexCenter} ${mt ? 'mt-4' : 'mb-4'}`}>
       <img data-for={`Player-${mt ? '1' : '2'}`} data-tip src={playerIcon} alt="player2" className='w-14 h-14 object-contain rounded-full' />
 
       <div data-for={`Health-${mt ? '1' : '2'}`}
